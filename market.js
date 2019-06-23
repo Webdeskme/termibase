c75198405be6b184658683d896ec4af85a94f1a0
Adam Telford <webdesk@pop-os.localdomain>
2019-06-22 21:50:01 -0400

    Update market

--- market.js
+++ market.js
@@ -1,3 +1,4 @@
-console.log("Our market place can be found at: https://webfra.me");
+console.log("Our market place can be found at: http://termibase.webfra.me");
 console.log("");
 console.log("To install just download the desired app into your Termibase folder in the Documents folder.");
+console.log("The finish the install with command 'termibase_install'");

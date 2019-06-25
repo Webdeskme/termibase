a4d1d0eb9fe2e24a56602368319de28817dd4c99
Adam Telford <webdesk@pop-os.localdomain>
2019-06-25 09:06:04 -0400

    Add first real market

--- install.js
+++ install.js
@@ -22,13 +22,13 @@ console.log("");
 console.log("Installing ...");
 console.log("");
 var thisPercentBar = new Progress(20);
-console.log(thisPercentBar.update(0.0));
+console.log(thisPercentBar.update(0.0) + " Opening TermiBase folder");
 fs.emptyDirSync(apps);
-console.log(thisPercentBar.update(0.1));
+console.log(thisPercentBar.update(0.1) "Cleard cache");
 fs.copySync(dh_term, apps);
-console.log(thisPercentBar.update(0.3));
+console.log(thisPercentBar.update(0.3) + " Copied apps");
 var files = fs.readdirSync(apps);
-console.log(thisPercentBar.update(0.4));
+console.log(thisPercentBar.update(0.4) + " Read apps");
 var con = "#!/usr/bin/env node\n";
 con += "const chalk = require('chalk');\n";
 con += "const clear = require('clear');\n";
@@ -48,7 +48,7 @@ con += "console.log('');\n";
 con += "console.log(chalk.cyan.bold('Welcome to TermiBase!'));\n";
 con += "console.log(chalk.yellow('Please type " + '"TermiBase -h"' + " to see all the installed commands and apps.'));\n";
 con += "console.log('');\n";
-con += "program.version('0.3.4');\n";
+con += "program.version('0.4.0');\n";
 con += "program.option('-m, --market', 'Market link of terminal apps.');\n";
 con += "program.option('-a, --add', 'Add an application from the market.');\n";
 con += "program.option('-n, --init', 'Start a new application.');\n";
@@ -61,7 +61,7 @@ con += "if (program.market){ require(__dirname + '/market.js');}\n";
 con += "else if (program.add) {require(__dirname + '/add.js');}\n";
 con += "else if (program.init) {require(__dirname + '/init.js');}\n";
 con += "else if (program.installer){ require(__dirname + '/installer.js');}\n";
-console.log(thisPercentBar.update(0.5));
+console.log(thisPercentBar.update(0.5) + " Started creating index");
 x = 0;
 for(x in files){
   con += "else if(program." + files[x] + "){require(apps + '" + files[x] + "');}\n";
@@ -89,11 +89,29 @@ con += "else if(answers.apps === '" + files[x] + "'){require(apps + '" + files[x
 }
 con += "});}";
 //console.log(con);
-console.log(thisPercentBar.update(0.6));
+console.log(thisPercentBar.update(0.6) + " Created index content");
 fs.copySync(__dirname + '/node_modules', apps + 'node_modules');
-console.log(thisPercentBar.update(0.9));
+console.log(thisPercentBar.update(0.8) + " Installed node_modules");
+const src = 'https://raw.githubusercontent.com/Webdeskme/termibase_market/master/market.json';
+const output = __dirname + '/market.json';
+const options = {};
+let download = wget.download(src, output, options);
+download.on('error', function(err) {
+console.log(err);
+});
+download.on('start', function(fileSize) {
+//console.log(fileSize);
+});
+download.on('end', function(output) {
+//console.log(output);
+});
+download.on('progress', function(progress) {
+typeof progress === 'number'
+// code to show progress bar
+console.log(thisPercentBar.update(9.0) + " Updated market");
+});
 fs.writeFile(__dirname + '/index.js', con, function (err) {
   if (err) throw err;
-  console.log(thisPercentBar.update(1.0));
+  console.log(thisPercentBar.update(1.0) + " Finishing");
   console.log('Installed!');
 });
--- market.js
+++ market.js
@@ -1,4 +1,32 @@
+const chalk       = require('chalk');
+const clear       = require('clear');
+const figlet      = require('figlet');
+const inquirer      = require('inquirer');
+var clui = require('clui');
+var Progress = clui.Progress;
+var thisPercentBar = new Progress(20);
+var fs = require('fs-extra');
+const wget = require('wget-improved');
+const dh_homedir = require('os').homedir();
+clear();
+console.log(
+  chalk.green(
+    figlet.textSync('TermiBase', { horizontalLayout: 'default' })
+  )
+);
+console.log("");
+console.log(chalk.cyan.bold('Welcome to TermiBase!'));
+console.log(chalk.yellow('Please type "TermiBase -h" to see all the installed commands and apps.'));
+console.log("");
 console.log("Our market place can be found at: http://termibase.webfra.me");
 console.log("");
 console.log("To install just download the desired app into your Termibase folder in the Documents folder.");
 console.log("The finish the install with command 'termibase_install'");
+console.log("");
+console.log("");
+var file = fs.readSync(__dirname + "/market.json");
+file = JSON.parse(file);
+var x;
+for(x in file){
+  console.log(x + ": " + file[x]["d"] " -- By: " + file[x]["a"] + "->v" + file[x]["v"]);
+}
